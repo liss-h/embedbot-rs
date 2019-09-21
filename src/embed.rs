c816daaf6b5c826983a245a51943f8b363c3a8dc
Liss <31625940+Clueliss@users.noreply.github.com>
2019-09-21 12:38:02 +0200

    split into 2 files

---
+++ src/embed.rs
@@ -0,0 +1,59 @@
+use crate::post_grab_api::Post;
+
+use serenity::builder::{CreateEmbed, CreateMessage};
+use serenity::model::channel::Message;
+
+const EMBED_CONTENT_MAX_LEN: usize = 2048;
+const EMBED_TITLE_MAX_LEN: usize = 256;
+
+pub fn escape_markdown(title: &str) -> String {
+    title
+        .replace("*", "\\*")
+        .replace("_", "\\_")
+        .replace("~", "\\~")
+        .replace(">", "\\>")
+}
+
+pub fn limit_len(text: &str, limit: usize) -> String {
+    const SHORTENED_MARKER: &str = " [...]";
+
+    if text.len() > limit {
+        format!("{}{}", &text[..(limit - SHORTENED_MARKER.len())], SHORTENED_MARKER)
+    } else {
+        text.to_string()
+    }
+}
+
+pub fn fmt_title(post: &Post) -> String {
+    let title = limit_len(
+        &escape_markdown(&post.title),
+        EMBED_TITLE_MAX_LEN - 9 - post.origin.len()); // -9 for formatting
+
+    format!("'{}' - **{}**", title, post.origin)
+}
+
+pub fn default_embed<'a>(e: &'a mut CreateEmbed, msg: &Message, post: &Post) -> &'a mut CreateEmbed {
+    e.title(&fmt_title(&post))
+        .description(&limit_len(&post.text, EMBED_CONTENT_MAX_LEN))
+        .author(|author_builder| author_builder.name(&msg.author.name))
+        .url(&msg.content)
+}
+
+pub fn image_embed(m: &mut CreateMessage, msg: &Message, post: &Post) {
+    m.embed(|e| default_embed(e, msg, post).image(&post.embed_url));
+}
+
+pub fn video_embed(m: &mut CreateMessage, msg: &Message, post: &Post) {
+    m.content(format!(
+        ">>> **{author}**\nSource: <{src}>\nEmbedURL: {embed_url}\n\n{title}\n\n{text}",
+        author = msg.author.name,
+        src = msg.content,
+        embed_url = post.embed_url,
+        title = fmt_title(&post),
+        text = limit_len(&post.text, EMBED_CONTENT_MAX_LEN),
+    ));
+}
+
+pub fn text_embed(m: &mut CreateMessage, msg: &Message, post: &Post) {
+    m.embed(|e| default_embed(e, msg, post));
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 
 extern crate serenity;
 
-use serenity::builder::{CreateEmbed, CreateMessage};
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
@@ -10,8 +9,8 @@ use serenity::prelude::*;
 mod post_grab_api;
 use post_grab_api::*;
 
-const EMBED_CONTENT_MAX_LEN: usize = 2048;
-const EMBED_TITLE_MAX_LEN: usize = 256;
+mod embed;
+use embed::*;
 
 
 fn is_url(url: &str) -> bool {
@@ -30,88 +29,26 @@ fn choose_grab_api(url: &str) -> Option<Box<dyn post_grab_api::PostGrabAPI>> {
     }
 }
 
-fn escape_markdown(title: &str) -> String {
-    title
-        .replace("*", "\\*")
-        .replace("_", "\\_")
-        .replace("~", "\\~")
-        .replace(">", "\\>")
-}
-
 fn should_embed(post: &Post) -> bool {
     (&post.website == "9gag" && post.post_type == PostType::Video)
         || &post.website != "9gag"
 }
 
-fn limit_len(text: &str, limit: usize) -> String {
-    const SHORTENED_MARKER: &str = " [...]";
-
-    if text.len() > limit {
-        format!("{}{}", &text[..(limit - SHORTENED_MARKER.len())], SHORTENED_MARKER)
-    } else {
-        text.to_string()
-    }
-}
-
-fn fmt_title(post: &Post) -> String {
-    let title = limit_len(
-        &escape_markdown(&post.title),
-        EMBED_TITLE_MAX_LEN - 9 - post.origin.len()); // -9 for formatting
-
-    format!("'{}' - **{}**", title, post.origin)
-}
-
-fn default_embed<'a>(msg: &Message, post: &Post, e: &'a mut CreateEmbed) -> &'a mut CreateEmbed {
-    e.title(&fmt_title(&post))
-        .description(&limit_len(&post.text, EMBED_CONTENT_MAX_LEN))
-        .author(|author_builder| author_builder.name(&msg.author.name))
-        .url(&msg.content)
-}
-
-fn embed_image(msg: &Message, ctx: &Context, post: &Post) -> Result<Message, serenity::Error> {
-    msg.channel_id
-        .send_message(&ctx.http, |m: &mut CreateMessage| {
-            m.embed(|e: &mut CreateEmbed| {
-                default_embed(&msg, &post, e)
-                    .image(&post.embed_url)
-            })
-        })
-}
-
-fn embed_video(msg: &Message, ctx: &Context, post: &Post) -> Result<Message, serenity::Error> {
-    msg.channel_id
-        .send_message(&ctx.http, |m: &mut CreateMessage| {
-            m.content(&format!(
-                ">>> **{author}**\nSource: <{src}>\nEmbedURL: {embed_url}\n\n{title}\n\n{text}",
-                author = msg.author.name,
-                src = msg.content,
-                embed_url = post.embed_url,
-                title = fmt_title(&post),
-                text = limit_len(&post.text, EMBED_CONTENT_MAX_LEN),
-            ))
-        })
-}
-
-fn embed_text(msg: &Message, ctx: &Context, post: &Post) -> Result<Message, serenity::Error> {
-    msg.channel_id
-        .send_message(&ctx.http, |m: &mut CreateMessage| {
-            m.embed(|e: &mut CreateEmbed| default_embed(&msg, &post, e))
-        })
-}
-
 fn send_embed_message(
-    msg: &Message,
     ctx: &Context,
+    msg: &Message,
     post: &Post,
 ) -> Result<Message, serenity::Error> {
-    match post.post_type {
-        PostType::Image => embed_image(&msg, &ctx, &post),
-        PostType::Video => match post.website.as_ref() {
-                    "reddit" => embed_image(&msg, &ctx, &post), // embed thumbnail
-                    _        => embed_video(&msg, &ctx, &post),
-                },
-        PostType::Text => embed_text(&msg, &ctx, &post)
-    }
+    msg.channel_id.send_message(ctx, |m| {
+        match post.post_type {
+            PostType::Image => image_embed(m, msg, post),
+            PostType::Text  => text_embed(m, msg, post),
+            PostType::Video if &post.website == "reddit" => image_embed(m, msg, post), // embed thumbnail
+            PostType::Video => video_embed(m, msg, post),
+        }
+
+        m
+    })
 }
 
 fn get_post(api: &mut dyn PostGrabAPI, url: &str) -> Result<Post, Error> {
@@ -131,7 +68,7 @@ impl EventHandler for EmbedBot {
             match choose_grab_api(&msg.content) {
                 Some(mut api) => match get_post(api.as_mut(), &msg.content) {
                     Ok(post) if should_embed(&post) => {
-                        send_embed_message(&msg, &context, &post).expect("could not send msg");
+                        send_embed_message(&context, &msg, &post).expect("could not send msg");
                         msg.delete(context.http).expect("could not delete msg");
 
                         println!("[Info] embedded '{}' as {:?}", msg.content, post.post_type);
